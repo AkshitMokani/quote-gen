5d3919151b8314be9fc733e660390088f80ff1e2
AkshitMokani <mokaniakshit@gmail.com>
2025-11-14 23:15:56 +0530

    Add redirect for Farcaster manifest JSON

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a8376-a264-9cf4-477e-9379700d7d1d',
+        permanent: false,  // This will make it a 307 Temporary Redirect
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
